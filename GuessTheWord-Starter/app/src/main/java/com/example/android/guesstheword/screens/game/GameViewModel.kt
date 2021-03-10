7412d0613ebeb6bb9a96d7a82cbb82708891ec25
Syizuril <syihabklayan@gmail.com>
2021-03-10 21:27:37 +0700

    add transformation livedata

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
@@ -15,17 +18,42 @@ class GameViewModel : ViewModel(){
         get() = _eventGameFinish
 
     // The current word
-    private var _word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
 
     // The current score
-    private var _score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime){ time ->
+        DateUtils.formatElapsedTime(time)
+    }
+
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+            "Current word has "+word.length+" letters"+"\n The letter at position "+randomPosition+" is "+
+                    word.get(randomPosition - 1).toUpperCase()
+    }
+
+    private val timer: CountDownTimer
+
+    companion object {
+        // Time when the game is over
+        private const val DONE = 0L
+
+        private const val ONE_SECOND = 1000L
+
+        private const val COUNTDOWN_TIME = 60000L
+    }
 
     /**
      * Resets the list of words and randomizes the order
@@ -62,7 +90,7 @@ class GameViewModel : ViewModel(){
      */
     fun nextWord() {
         if (wordList.isEmpty()) {
-            onGameFinish()
+            resetList()
         }else{
             _word.value = wordList.removeAt(0)
         }
@@ -86,11 +114,24 @@ class GameViewModel : ViewModel(){
         resetList()
         nextWord()
         Log.i("GameViewModel","GameViewModel Created!")
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onTick(milisUntilFinished: Long) {
+                _currentTime.value = milisUntilFinished/ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameVIewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
 
     fun onGameFinish(){
